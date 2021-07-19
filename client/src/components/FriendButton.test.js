fd835273f3c044a4abf9f20808e84b0d142582b5
Fernando Ardente <ardente@mailbox.org>
2021-07-19 12:48:31 +0200

    Added tests for clicking the button

--- client/src/components/FriendButton.test.js
+++ client/src/components/FriendButton.test.js
@@ -1,5 +1,5 @@
 import FriendButton from "./FriendButton";
-import { render, waitFor } from "@testing-library/react";
+import { render, waitFor, fireEvent } from "@testing-library/react";
 import axios from "../axios";
 
 jest.mock("../axios");
@@ -10,16 +10,28 @@ test("Button is not shown for own profile", async () => {
     await waitFor(() => expect(container.querySelector("button")).toBeNull());
 });
 
-test("Button shows Add Friend whithout friend request for other profiles", async () => {
+test("Button shows Add Friend whithout friend request for other profiles, onClick changes to Cancel Request", async () => {
     axios.get.mockResolvedValue({ data: { user_id: 1 } });
+    axios.post.mockResolvedValue({
+        data: { user_id: 1, sender_id: 1, receiver_id: 2, accepted: false },
+    });
     const { container } = render(<FriendButton other_id="2" />);
-    await waitFor(() =>
-        expect(container.querySelector("button").innerHTML).toBe("Add Friend")
-    );
+    await waitFor(() => {
+        expect(container.querySelector("button").innerHTML).toBe("Add Friend");
+    });
+    fireEvent.click(container.querySelector("button"));
+    await waitFor(() => {
+        expect(container.querySelector("button").innerHTML).toBe(
+            "Cancel Request"
+        );
+    });
 });
 
-test("Button shows Cancel Request with friend request when Sender is viewing", async () => {
+test("Button shows Cancel Request with friend request when Sender is viewing, onClick changes to Add Friend", async () => {
     axios.get.mockResolvedValue({
+        data: { user_id: 1, sender_id: 1, receiver_id: 2, accepted: false },
+    });
+    axios.post.mockResolvedValue({
         data: { user_id: 1, sender_id: 1, receiver_id: 2, accepted: false },
     });
     const { container } = render(<FriendButton other_id="2" />);
@@ -28,11 +40,18 @@ test("Button shows Cancel Request with friend request when Sender is viewing", a
             "Cancel Request"
         )
     );
+    fireEvent.click(container.querySelector("button"));
+    await waitFor(() => {
+        expect(container.querySelector("button").innerHTML).toBe("Add Friend");
+    });
 });
 
-test("Button shows Accept Friend with friend request when Receiver is viewing", async () => {
+test("Button shows Accept Friend with friend request when Receiver is viewing, onClick changes to Unfriend", async () => {
     axios.get.mockResolvedValue({
         data: { user_id: 2, sender_id: 1, receiver_id: 2, accepted: false },
+    });
+    axios.post.mockResolvedValue({
+        data: { user_id: 1, sender_id: 1, receiver_id: 2, accepted: true },
     });
     const { container } = render(<FriendButton other_id="1" />);
     await waitFor(() =>
@@ -40,14 +59,25 @@ test("Button shows Accept Friend with friend request when Receiver is viewing", 
             "Accept Friend"
         )
     );
+    fireEvent.click(container.querySelector("button"));
+    await waitFor(() => {
+        expect(container.querySelector("button").innerHTML).toBe("Unfriend");
+    });
 });
 
-test("Button shows Unfriend whit accepted friend request", async () => {
+test("Button shows Unfriend whit accepted friend request, onClick changes to Add Friend", async () => {
     axios.get.mockResolvedValue({
+        data: { user_id: 1, sender_id: 1, receiver_id: 2, accepted: true },
+    });
+    axios.post.mockResolvedValue({
         data: { user_id: 1, sender_id: 1, receiver_id: 2, accepted: true },
     });
     const { container } = render(<FriendButton other_id="2" />);
     await waitFor(() =>
         expect(container.querySelector("button").innerHTML).toBe("Unfriend")
     );
+    fireEvent.click(container.querySelector("button"));
+    await waitFor(() => {
+        expect(container.querySelector("button").innerHTML).toBe("Add Friend");
+    });
 });
