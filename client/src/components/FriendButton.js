b5faccc5d281f8454ba3be466253d7e6a7eb8d82
Fernando Ardente <ardente@mailbox.org>
2021-07-19 12:43:32 +0200

    Added eventhandler for updating friendship

--- client/src/components/FriendButton.js
+++ client/src/components/FriendButton.js
@@ -7,13 +7,16 @@ export default function FriendButton({ other_id }) {
     const ACCEPT_REQUEST = "Accept Friend";
     const UNFRIEND = "Unfriend";
 
+    let [friendship_id, setFriendship_id] = useState(null);
+    let [user_id, setUser_id] = useState(null);
     let [buttonText, setButtonText] = useState(ADD_FRIEND);
     let [showButton, setShowButton] = useState(true);
-    console.log("friend button other id", other_id);
+
     useEffect(() => {
-        console.log("mounting friend button, other_id: ", other_id);
         axios.get("/api/user/" + other_id + "/requests").then((result) => {
-            let { user_id, sender_id, receiver_id, accepted } = result.data;
+            let { id, user_id, sender_id, receiver_id, accepted } = result.data;
+            setUser_id(user_id);
+            setFriendship_id(id);
 
             if (user_id == other_id) {
                 setShowButton(false);
@@ -39,10 +42,53 @@ export default function FriendButton({ other_id }) {
             }
         });
     }, [other_id]);
+
+    async function onClick() {
+        switch (buttonText) {
+            case ADD_FRIEND: {
+                const response = await axios.post("/api/user/addfriend", {
+                    sender_id: user_id,
+                    receiver_id: other_id,
+                });
+                setButtonText(CANCEL_REQUEST);
+                break;
+            }
+            case CANCEL_REQUEST: {
+                const response = await axios.post(
+                    "/api/user/deletefriendship",
+                    {
+                        id: friendship_id,
+                    }
+                );
+                setButtonText(ADD_FRIEND);
+                break;
+            }
+            case ACCEPT_REQUEST: {
+                const response = await axios.post("/api/user/acceptfriend", {
+                    id: friendship_id,
+                });
+                setButtonText(UNFRIEND);
+                break;
+            }
+            case UNFRIEND: {
+                const response = await axios.post(
+                    "/api/user/deletefriendship",
+                    {
+                        id: friendship_id,
+                    }
+                );
+                setButtonText(ADD_FRIEND);
+                break;
+            }
+        }
+    }
+
     return (
         <div>
             {showButton && (
-                <button className="friendButton">{buttonText}</button>
+                <button className="friendButton" onClick={onClick}>
+                    {buttonText}
+                </button>
             )}
         </div>
     );
