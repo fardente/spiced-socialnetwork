a0a1f110a5eb0078755454f483c99634649b7675
Fernando Ardente <ardente@mailbox.org>
2021-07-22 20:26:39 +0200

    Added Chat-Table

--- sql/socialnetwork.sql
+++ sql/socialnetwork.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS chat;
 DROP TABLE IF EXISTS friends;
 DROP TABLE IF EXISTS passreset;
 DROP TABLE IF EXISTS users;
@@ -14,10 +15,17 @@ CREATE TABLE users(
 );
 
 CREATE TABLE friends(
-    id SERiaL PRiMARY KEY,
+    id SERIAL PRIMARY KEY,
     sender_id INT REFERENCES users (id) NOT NULL,
     receiver_id INT REFERENCES users (id) NOT NULL,
     accepted BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE chat(
+    id SERIAL PRIMARY KEY,
+    sender_id INT REFERENCES users (id) NOT NULL,
+    message VARCHAR NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -32,3 +40,12 @@ INSERT INTO users (firstname, lastname, email, passwordhash) VALUES ('Homer', 'S
 INSERT INTO users (firstname, lastname, email, passwordhash) VALUES ('Marge', 'Simpson', 'marge@s.com', '$2a$10$vPIxWGr70UNqekKXnLOnwewIb0euur0w2k2MS/LHBbGwlxecSo3lm');
 INSERT INTO users (firstname, lastname, email, passwordhash) VALUES ('Bart', 'Simpson', 'bart@s.com', '$2a$10$vPIxWGr70UNqekKXnLOnwewIb0euur0w2k2MS/LHBbGwlxecSo3lm');
 INSERT INTO users (firstname, lastname, email, passwordhash) VALUES ('Lisa', 'Simpson', 'lisa@s.com', '$2a$10$vPIxWGr70UNqekKXnLOnwewIb0euur0w2k2MS/LHBbGwlxecSo3lm');
+
+INSERT INTO friends (sender_id, receiver_id, accepted) VALUES (1, 2, true);
+INSERT INTO friends (sender_id, receiver_id, accepted) VALUES (1, 3, false);
+INSERT INTO friends (sender_id, receiver_id, accepted) VALUES (1, 4, true);
+INSERT INTO friends (sender_id, receiver_id, accepted) VALUES (2, 3, true);
+INSERT INTO friends (sender_id, receiver_id, accepted) VALUES (2, 4, true);
+INSERT INTO friends (sender_id, receiver_id, accepted) VALUES (4, 3, false);
+
+INSERT INTO chat (sender_id, message) VALUES (1, 'mmhhmm Donuts...');
