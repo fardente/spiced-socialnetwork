79df28f5f2d0d111be336460defaf1a689e130f1
Fernando Ardente <ardente@mailbox.org>
2021-10-06 10:59:10 +0200

    Prepare for Heroku deployment

--- server/ses.js
+++ server/ses.js
@@ -1,9 +1,11 @@
 const aws = require("aws-sdk");
-const secrets = require("./secrets");
+const AWS_KEY = process.env.AWS_KEY || require("./secrets").AWS_KEY;
+
+const AWS_SECRET = process.env.AWS_SECRET || require("./secrets").AWS_SECRET;
 
 const ses = new aws.SES({
-    accessKeyId: secrets.AWS_KEY,
-    secretAccessKey: secrets.AWS_SECRET,
+    accessKeyId: AWS_KEY,
+    secretAccessKey: AWS_SECRET,
     region: "eu-central-1",
 });
 
